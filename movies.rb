06e662e23601a921edb934945071d92744e6b557
MadYara <45680174+MadYara@users.noreply.github.com>
2018-12-07 15:11:03 +0300

    Add files via upload
    task 1

---
+++ movies.rb
@@ -0,0 +1,10 @@
+input_array = ARGV
+a = ARGV[0]
+ if a == "Titanic"
+ 	puts "Titanic is a bad movie"
+ elsif a == "Matrix"
+ 	puts "Matrix is a good movie"
+ else 
+ 	puts "Haven't seen #{a} yet"
+ end
+
